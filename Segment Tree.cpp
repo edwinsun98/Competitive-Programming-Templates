c9a1330fb078f75531476247e3a87ad89c0ad79d
edwinsun98 <57502912+edwinsun98@users.noreply.github.com>
2021-06-08 21:18:34 -0400

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,44 +1,44 @@
-struct seg{
+template<typename T> struct seg{
+    struct node{
+        int l, r;
+        T sum;
+    };
+    vector<node> seg;
     int n;
-    ll fun(ll x, ll y){return x+y;}
-    ll seg[4000010], arr[1000010];
-    ll build(int cur, int l, int r){
-        if(l == r){
-            seg[cur] = arr[l];
-        }
-        else{
-            int mid = (l+r)/2;
-            build(cur*2, l, mid); build(cur*2+1, mid+1, r);
-            seg[cur] = fun(seg[cur*2], seg[cur*2+1]);
-        }
+    void init(int sz){
+        n = sz;
+        int accurate = (1<<((int)log2(n)+2))-1;
+        seg.assign(accurate, (node){0, 0, 0});
     }
-    ll update_(int cur, int l, int r, int idx, ll val){
-        if(l == r){
-            seg[cur] += val; arr[idx] += val;
-        }
-        else{
-            int mid = (l+r)/2;
-            if(idx >= l && idx <= mid)update_(cur*2, l, mid, idx, val);
-            else update_(cur*2+1, mid+1, r, idx, val);
-            seg[cur] = fun(seg[cur*2], seg[cur*2+1]);
-        }
+    // TODO: operation change
+    void pushup(int rt){
+        seg[rt].sum = max(seg[2*rt].sum , seg[2*rt+1].sum);
     }
-    ll update(int idx, ll val){
-        return update_(1, 1, n, idx, val);
-    }
-    ll query_(int cur, int l, int r, int ql, int qr){
-        if(qr < l || r < ql){
-            return (ll)0;
+    void build(int l, int r, int rt){
+        seg[rt].l = l; seg[rt].r = r;
+        if(l == r) {
+//            cin>>seg[rt].sum;
+            return;
         }
-        if(ql <= l && r <= qr){
-            return seg[cur];
-        }
-        int mid = (l + r) / 2;
-        ll p1 = query_(2*cur,l, mid, ql, qr), p2 = query_(2*cur+1, mid+1,r, ql, qr);
-        return fun(p1 ,p2);
+        int mid = (l + r)/2;
+        build(l, mid, 2*rt); build(mid+1, r, 2*rt+1); pushup(rt);
     }
-    ll query(int l, int r){
-        return query_(1, 1, n, l, r);
+    void update(int id, T val, int rt){
+        if(seg[rt].l == seg[rt].r){
+            seg[rt].sum = val;
+            return;
+        }
+        int mid = (seg[rt].l + seg[rt].r)/2;
+        if(id <= mid) update(id, val, 2*rt);
+        else update(id, val, 2*rt+1);
+        pushup(rt);
+    }
+    T query(int l, int r, int rt){
+        if(seg[rt].l == l && seg[rt].r == r) return seg[rt].sum;
+        int mid = (seg[rt].l + seg[rt].r)/2;
+        if(r <= mid) return query(l, r, 2*rt);
+        if(l > mid) return query(l, r, 2*rt+1);
+        // TODO: operation change
+        return max(query(l, mid, 2*rt) , query(mid+1, r, 2*rt+1));
     }
 };
-/*---------Segment Tree End---------*/
